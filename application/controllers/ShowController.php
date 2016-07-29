ed47beb06326ee3298ebdc18f0ab9e749d08079b
Thomas Gelf <thomas@gelf.net>
2016-07-29 07:35:07 +0000

    show/trap: should look better

--- application/controllers/ShowController.php
+++ application/controllers/ShowController.php
@@ -9,9 +9,9 @@ class ShowController extends Controller
 {
     public function trapAction()
     {
-        $this->view->trap = Trap::load($this->params->get('id'), $this->db());
+        $this->view->trap = $trap = Trap::load($this->params->get('id'), $this->db());
         $this->getTabs()->add('trap', array(
-            'label' => $this->view->trap->short_name,
+            'label' => $trap->getOidName($trap->oid),
             'url'   => $this->getRequest()->getUrl())
         )->activate('trap');
     }
