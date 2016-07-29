7834f94b204bff1c22496a24b87b055c5d23c92d
Thomas Gelf <thomas@gelf.net>
2016-07-29 07:17:23 +0000

    SummaryTables: fix count, show "better" links

--- application/tables/HostSummaryTable.php
+++ application/tables/HostSummaryTable.php
@@ -25,8 +25,98 @@ class HostSummaryTable extends QuickTable
             'source'      => "CASE WHEN t.host_name IS NULL THEN t.src_address ELSE t.host_name || ' (' || t.src_address || ')' END",
             'src_address' => 't.src_address',
             'host_name'   => 't.host_name',
-            'trap_count'  => 'COUNT(*)',
+            'trap_count'  => 'COUNT(*)'
         );
+    }
+
+    protected function renderSourceColumn($row)
+    {
+        $htm = '';
+        if ($row->host_name) {
+            $htm .= $this->view()->qlink(
+                $row->host_name,
+                'trappola/list/traps',
+                array(
+                    'host_name'   => $row->host_name,
+                )
+            );
+
+        }
+
+        if ($row->src_address) {
+            if ($row->host_name) {
+                $htm .= ' (';
+            }
+
+            $htm .= $this->view()->qlink(
+                $row->src_address,
+                'trappola/list/traps',
+                array(
+                'src_address' => $row->src_address,
+                )
+            );
+
+            if ($row->host_name) {
+                $htm .= ')';
+            }
+        }
+
+        return $htm;
+    }
+
+    public function count()
+    {
+        $db = $this->connection()->getConnection();
+        $sub = clone($this->getBaseQuery());
+        $sub->columns($this->getColumns());
+        $this->applyFiltersToQuery($sub);
+        $query = $db->select()->from(
+            array('sub' => $sub),
+            'COUNT(*)'
+        );
+
+        return $db->fetchOne($query);
+    }
+
+    protected function renderRow($row)
+    {
+        $htm = "  <tr" . $this->getRowClassesString($row) . ">\n";
+        $firstCol = true;
+
+        foreach ($this->getTitles() as $key => $title) {
+
+            // Support missing columns
+            if (property_exists($row, $key)) {
+                $val = $row->$key;
+            } else {
+                $htm .= "    <td>-</td>\n";
+                continue;
+            }
+
+            $func = 'render' . ucfirst($key) . 'Column';
+            if (method_exists($this, $func)) {
+                $value = $this->$func($row);
+                $firstCol = false;
+            } elseif ($firstCol) {
+                if ($url = $this->getActionUrl($row)) {
+                    $value = $this->view()->qlink($val, $this->getActionUrl($row));
+                } else {
+                    $value  = $val;
+                }
+
+                $firstCol = false;
+            } else {
+                $value = $this->view()->escape($val);
+            }
+
+            $htm .= '    <td>' . $value . "</td>\n";
+        }
+
+        if ($this->hasAdditionalActions()) {
+            $htm .= '    <td class="actions">' . $this->renderAdditionalActions($row) . "</td>\n";
+        }
+
+        return $htm . "  </tr>\n";
     }
 
     protected function getActionUrl($row)
@@ -59,7 +149,8 @@ class HostSummaryTable extends QuickTable
         $query = $db->select()->from(
             array('t' => 'trap'),
             array()
-        )->where('t.timestamp > ?', date('Y-m-d H:i:s', $this->start))
+        )//->where('t.timestamp > ?', date('Y-m-d H:i:s', $this->start))
+         //->where('t.acknowledged = ?', 'y')
          ->group('t.src_address')
          ->group('t.host_name')
          ->order('COUNT(*) DESC')
--- application/tables/TrapSummaryTable.php
+++ application/tables/TrapSummaryTable.php
@@ -22,10 +22,31 @@ class TrapSummaryTable extends QuickTable
     public function getColumns()
     {
         return array(
-            'trap_name'   => "c.mib_name || '::' || c.short_name",
-            'oid'         => 't.oid',
-            'trap_count'  => 'COUNT(*)',
+            'trap_name'    => "c.mib_name || '::' || c.short_name",
+            'oid'          => 't.oid',
+            'trapcount'    => 'COUNT(*)',
+            'unacknowledged' => "SUM(CASE WHEN t.acknowledged = 'n' THEN 1 ELSE 0 END)"
         );
+    }
+
+    protected function renderTrapcountColumn($row)
+    {
+        if  ((int) $row->trapcount === 0) {
+            return '-';
+        }
+
+        $parts = array();
+        $trapState = '<span class="%s">%d</span>';
+        $ok = $row->trapcount - $row->unacknowledged;
+        if ($ok > 0) {
+            $parts[] = sprintf($trapState, 'ok', $ok);
+        }
+
+        if ($row->unacknowledged > 0) {
+            $parts[] = sprintf($trapState, 'critical', $row->unacknowledged);
+        }
+
+        return implode(' / ', $parts);
     }
 
     protected function getActionUrl($row)
@@ -37,9 +58,23 @@ class TrapSummaryTable extends QuickTable
     {
         $view = $this->view();
         return array(
-            'trap_name'   => $view->translate('Trap'),
-            'trap_count' => $view->translate('Count'),
+            'trap_name'      => $view->translate('Trap'),
+            'trapcount'     => $view->translate('Count'),
         );
+    }
+
+    public function count()
+    {
+        $db = $this->connection()->getConnection();
+        $sub = clone($this->getBaseQuery());
+        $sub->columns($this->getColumns());
+        $this->applyFiltersToQuery($sub);
+        $query = $db->select()->from(
+            array('sub' => $sub),
+            'COUNT(*)'
+        );
+
+        return $db->fetchOne($query);
     }
 
     public function getBaseQuery()
@@ -54,11 +89,52 @@ class TrapSummaryTable extends QuickTable
             array('c' => 'trap_oidcache'),
             'c.oid = t.oid',
             array()
-        )->where('t.timestamp > ?', date('Y-m-d H:i:s', $this->start))
-         ->group('t.oid')
+        )//->where('t.timestamp > ?', date('Y-m-d H:i:s', $this->start))
+         ->group('t.oid_checksum')
          ->order('c.mib_name ASC')
          ->order('c.short_name ASC');
 
         return $query;
+    }
+
+    protected function renderRow($row)
+    {
+        $htm = "  <tr" . $this->getRowClassesString($row) . ">\n";
+        $firstCol = true;
+
+        foreach ($this->getTitles() as $key => $title) {
+
+            // Support missing columns
+            if (property_exists($row, $key)) {
+                $val = $row->$key;
+            } else {
+                $htm .= "    <td>-</td>\n";
+                continue;
+            }
+
+            $func = 'render' . ucfirst($key) . 'Column';
+            if (method_exists($this, $func)) {
+                $value = $this->$func($row);
+                $firstCol = false;
+            } elseif ($firstCol) {
+                if ($url = $this->getActionUrl($row)) {
+                    $value = $this->view()->qlink($val, $this->getActionUrl($row));
+                } else {
+                    $value  = $val;
+                }
+
+                $firstCol = false;
+            } else {
+                $value = $this->view()->escape($val);
+            }
+
+            $htm .= '    <td>' . $value . "</td>\n";
+        }
+
+        if ($this->hasAdditionalActions()) {
+            $htm .= '    <td class="actions">' . $this->renderAdditionalActions($row) . "</td>\n";
+        }
+
+        return $htm . "  </tr>\n";
     }
 }
