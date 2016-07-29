3e5d6f18f70f9da9a7ab5367805389c2a993c2e1
Thomas Gelf <thomas@gelf.net>
2016-07-29 07:00:00 +0000

    schema/mysql: provide a couple of changes

--- schema/mysql-legacy.sql
+++ schema/mysql-legacy.sql
@@ -92,3 +92,38 @@ CREATE TABLE trap_varbind (
   CONSTRAINT trap_varbind_ibfk_1 FOREIGN KEY (trap_id) REFERENCES trap (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- TODO: add real migrations
+
+alter table trap modify short_name varchar(64) null default null;
+alter table trap modify mib_name varchar(64) null default null;
+alter table trap modify timestamp timestamp not null default current_timestamp;
+
+ALTER TABLE trap ADD COLUMN acknowledged ENUM('y', 'n') NOT NULL DEFAULT 'n' AFTER short_name;
+ALTER TABLE trap ADD INDEX idx_host_ack (host_name(64), acknowledged);
+ALTER TABLE trap ADD INDEX idx_ts_ack (timestamp, acknowledged);
+
+
+UPDATE trap t JOIN trap_oidcache c ON t.oid = c.oid SET t.mib_name = c.mib_name, t.short_name = c.short_name, t.message = c.description WHERE (t.short_name LIKE '%.%.%' OR t.short_name IS NULL) AND c.short_name NOT LIKE '%.%.%'
+
+ALTER TABLE trap
+  ADD COLUMN oid_checksum VARBINARY(20) NOT NULL AFTER messageid,
+  ADD INDEX oid_checksum (oid_checksum);
+-- Query OK, 35536 rows affected (1.73 sec)
+
+UPDATE trap SET oid_checksum = UNHEX(SHA1(oid));
+-- Query OK, 35536 rows affected (1.31 sec)
+
+ALTER TABLE trap_varbind
+  ADD COLUMN oid_checksum VARBINARY(20) NOT NULL AFTER trap_id,
+  ADD INDEX oid_checksum (oid_checksum);
+-- Query OK, 1228915 rows affected (18.07 sec)
+
+UPDATE trap_varbind SET oid_checksum = UNHEX(SHA1(oid));
+-- Query OK, 1228915 rows affected (1 min 32.57 sec)
+
+
+ALTER TABLE trap_oidcache
+  ADD COLUMN oid_checksum VARBINARY(20) NOT NULL AFTER oid,
+  ADD INDEX oid_checksum (oid_checksum);
+
+UPDATE trap_oidcache SET oid_checksum = UNHEX(SHA1(oid));
