2d0cda05dbcc035bdf553ba802efbdd05b11df8d
Thomas Gelf <thomas@gelf.net>
2016-07-29 07:06:55 +0000

    Redis: allow to configure host and port

--- library/Trappola/Redis.php
+++ library/Trappola/Redis.php
@@ -3,6 +3,7 @@
 namespace Icinga\Module\Trappola;
 
 use Predis\Client as PredisClient;
+use Icinga\Application\Config;
 
 class Redis
 {
@@ -12,7 +13,13 @@ class Redis
     {
         if (self::$redis === null) {
             require_once dirname(__DIR__) . '/vendor/predis/autoload.php';
-            self::$redis = new PredisClient();
+
+            // TODO: get rid of static handling
+            $config = Config::module('trappola');
+            self::$redis = new PredisClient(array(
+                'host' => $config->get('redis', 'host', '127.0.0.1'),
+                'port' => (int) $config->get('redis', 'port', 6379),
+            ));
         }
 
         return self::$redis;
