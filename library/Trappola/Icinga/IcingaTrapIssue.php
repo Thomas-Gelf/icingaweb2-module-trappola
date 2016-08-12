1eb4459c835721bfba00573ddab54be8ed10418c
Thomas Gelf <thomas@gelf.net>
2016-08-12 12:22:40 +0000

    IcingaTrapIssue: new issue abstraction

---
+++ library/Trappola/Icinga/IcingaTrapIssue.php
@@ -0,0 +1,247 @@
+<?php
+
+namespace Icinga\Module\Trappola\Icinga;
+
+use Icinga\Module\Trappola\Data\Db\DbObject;
+use Icinga\Module\Trappola\Handler\TrapHandler;
+use Icinga\Module\Trappola\Trap;
+use Icinga\Module\Trappola\TrapDb;
+
+class IcingaTrapIssue extends DbObject
+{
+    // host!service
+    protected $table = 'icinga_trap_issue';
+
+    protected $keyName = 'checksum';
+
+    protected $relatedIssues;
+    
+    protected $defaultProperties = array(
+        'id'                     => null,
+        // TODO: traphandler is not yet part of the checksum, should be:
+        // sha1(icinga_object!trap_handler!remote_identifier)
+        'checksum'               => null,
+        'icinga_object'          => null,
+        'icinga_object_checksum' => null,
+        'icinga_host'            => null,
+        'icinga_service'         => null,
+        'first_event'            => null, // timestamp
+        'last_event'             => null, // timestamp
+        'cnt_events'             => null,
+        'icinga_state'           => null,
+        'message'                => null,
+        // 'trap_handler'        => null,
+        // 'remote_identifier'   => null,
+        'expire_after'           => null, // expire the problem
+    );
+
+    protected static $icingaServiceStates = array(
+        'OK',
+        'WARNING',
+        'CRITICAL',
+        'UNKNOWN'
+    );
+
+    public static function handleTrap(TrapHandler $handler, Trap $trap)
+    {
+        if (! $handler->isIcingaIssue()) {
+            return;
+        }
+        
+        $db = $trap->getConnection();
+        $key = $handler->getIcingaObjectName();
+        $identifier = $handler->getIssueIdentifier();
+        $checksum = sha1($identifier, true);
+        $objectName = $handler->getIcingaObjectname();
+
+        if (self::exists($checksum, $db)) {
+            $issue = self::load($checksum, $db);
+        } else {
+            $issue = static::create(array(
+                'checksum'               => $checksum,
+                'icinga_object'          => $objectName,
+                'icinga_object_checksum' => sha1($objectName, true),
+                'icinga_host'            => $handler->getIcingaHostname(),
+                'icinga_service'         => $handler->getIcingaServicename(),
+                'first_event'            => self::now(),
+            ), $db);
+        }
+
+        return $issue->processTrapProperties($handler, $trap);
+    }
+
+    protected function icingaServiceStateName($state)
+    {
+        return self::$icingaServiceStates[$state];
+    }
+
+    protected function processTrapProperties(TrapHandler $handler, Trap $trap)
+    {
+        $this->cnt_events   = (int) $this->cnt_events + 1;
+        $this->message      = $trap->message;
+        $this->expire_after = null;
+        $this->last_event   = self::now();
+        $this->icinga_state = $handler->getIcingaState();
+
+        return $this->storeAndRefreshIcinga();
+    }
+
+    protected function storeAndRefreshIcinga()
+    {
+        if ($this->hasBeenModified()) {
+            $pipe = new IcingaCommandPipe();
+            $this->store();
+            $pipe->sendIssue($this);
+        }
+
+        return $this;
+    }
+
+    protected function fetchRelatedIssues()
+    {
+        if ($this->relatedIssues === null) {
+            $db = $this->getConnection()->getDbAdapter();
+
+            $query = $db->select()->from(
+                array('i' => $this->table),
+                array(
+                    'icinga_state',
+                    'expire_after',
+                    'message',
+                )
+            )->where('i.icinga_object_checksum = ?', $this->icinga_object_checksum);
+
+            $this->relatedIssues = $db->fetchAll($query);
+        }
+
+        return $this->relatedIssues;
+    }
+
+    public static function listForIcingaService($host, $service, TrapDb $connection)
+    {
+        $db = $connection->getDbAdapter();
+
+        $checksum = sha1($host . '!' . $service, 1);
+
+        $query = $db->select()->from(
+                array('i' => 'icinga_trap_issue'),
+                array(
+                    'checksum'         => 'LOWER(HEX(i.checksum))',
+                    'icinga_state'     => 'i.icinga_state',
+                    'message'          => 'i.message',
+                    'first_event'      => 'i.first_event',
+                    'last_event'       => 'i.last_event',
+                    'cnt_events'       => 'i.cnt_events',
+                    'expire_after'     => 'i.expire_after',
+                )
+            )->where('i.icinga_object_checksum = ?', $checksum);
+
+        return $db->fetchAll($query);
+    }
+
+    public static function existsForIcingaService($host, $service, TrapDb $connection)
+    {
+        $db = $connection->getDbAdapter();
+
+        $checksum = sha1($host . '!' . $service, 1);
+
+        $query = $db->select()->from(
+                array('i' => 'icinga_trap_issue'),
+                array('cnt' => 'COUNT(*)')
+            )->where('i.icinga_object_checksum = ?', $checksum);
+
+        return $db->fetchOne($query) > 0;
+    }
+
+    public static function loadForHexChecksum($checksum, TrapDb $db)
+    {
+        return static::load(pack('H*', $checksum), $db);
+    }
+
+    public function expire($author)
+    {
+        $this->message = sprintf(
+            'Manually expired by %s. Was: ',
+            $author
+        ) . $this->message;
+ 
+        $this->expire_after = date('Y-m-d H:i:s', time() + 1800);
+        return $this->storeAndRefreshIcinga();
+    }
+
+    public function getConsolidatedOutput()
+    {
+        $list = array();
+        foreach ($this->fetchRelatedIssues() as $issue) {
+            if ($issue->expire_after !== null) {
+                continue;
+            }
+
+            $list[] = sprintf(
+                '[%s] %s',
+                strtoupper($this->icingaServiceStateName((int) $issue->icinga_state)),
+                $issue->message
+            );
+        }
+
+        if (empty($list)) {
+            return null;
+        }
+
+        return implode("\n", $list) . "\n";
+    }
+
+    public function getWorstState()
+    {
+        $state = $this->fakeStateForIssue($this);
+        foreach ($this->fetchRelatedIssues() as $issue) {
+            $state = max($state, $this->fakeStateForIssue($issue));
+        }
+
+        return $this->restoreLoweredUnknown($state);
+    }
+
+    protected function fakeStateForIssue($issue)
+    {
+        if ($issue->expire_after === null) {
+            $state = $issue->icinga_state;
+        } else {
+            $state = 0;
+        }
+
+        return $this->fakeLowerUnknown($state);
+    }
+
+    protected function raiseState($state)
+    {
+        if ($this->fakeLowerUnknown($state) > $this->fakeLowerUnknown($this->icinga_state)) {
+            $this->icinga_state = (int) $state;
+        }
+
+        return $this;
+    }
+
+    protected function fakeLowerUnknown($state)
+    {
+        $state = (int) $state;
+        if ($state === 3) {
+            return 1.5;
+        }
+
+        return $state;
+    }
+
+    protected function restoreLoweredUnknown($state)
+    {
+        if ($state === 1.5) {
+            return 3;
+        }
+
+        return $state;
+    }
+
+    protected static function now()
+    {
+        return date('Y-m-d H:i:s');
+    }
+}
