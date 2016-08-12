b8a6bb219975785361a3896e891d18a7154e78cf
Thomas Gelf <thomas@gelf.net>
2016-08-12 12:22:08 +0000

    schema/mysql: add icinga_trap_issue table

--- schema/mysql-legacy.sql
+++ schema/mysql-legacy.sql
@@ -127,3 +127,22 @@ ALTER TABLE trap_oidcache
   ADD INDEX oid_checksum (oid_checksum);
 
 UPDATE trap_oidcache SET oid_checksum = UNHEX(SHA1(oid));
+
+CREATE TABLE icinga_trap_issue (
+  id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  checksum VARBINARY(20) NOT NULL,
+  icinga_object VARCHAR(255) DEFAULT NULL,
+  icinga_object_checksum VARBINARY(20) NOT NULL,
+  icinga_host VARCHAR(255) NOT NULL,
+  icinga_service VARCHAR(255) NOT NULL,
+  first_event DATETIME NOT NULL,
+  last_event DATETIME NOT NULL,
+  cnt_events INT(10) UNSIGNED NOT NULL,
+  icinga_state TINYINT UNSIGNED NOT NULL,
+  message TEXT DEFAULT NULL,
+  expire_after DATETIME DEFAULT NULL,
+  PRIMARY KEY(id),
+  UNIQUE KEY(checksum),
+  KEY icinga_object_checksum (icinga_object_checksum),
+  KEY icinga_host (icinga_host)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
